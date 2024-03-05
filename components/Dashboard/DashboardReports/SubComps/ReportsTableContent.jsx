3b1a5282db3fb12e639a6def8a79ff96481d5c25
projesh-BVS <144583856+projesh-BVS@users.noreply.github.com>
2024-03-05 10:25:01 +0530

    [Product Names In Report Table] v 0.9.5 Updated
    Added product names to report table

--- components/Dashboard/DashboardReports/SubComps/ReportsTableContent.jsx
+++ components/Dashboard/DashboardReports/SubComps/ReportsTableContent.jsx
@@ -5,6 +5,7 @@ import { useMemo, useState } from "react";
 import DataTable from "react-data-table-component";
 
 const columns = [
+  { name: "Product Name", selector: (row) => row.productName, sortable: true },
   { name: "Product ID", selector: (row) => row.productID, sortable: true },
   { name: "Product SKU", selector: (row) => row.productSKU, sortable: true },
   { name: "Views 360", selector: (row) => row.views360, sortable: true },
